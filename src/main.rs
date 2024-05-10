83c38a1cea1acca4d4c070045339d0b6410bf034
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-10 20:21:44 +0200

    Single creation of generator.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-mod animals;
+use rand::Rng;
+pub mod animals;
 
 fn main() {
+    let mut rng = rand::thread_rng();
+
     let mut herb = animals::Herbivore {
         animal: animals::Animal {
             weight: 5.0,
