89a3afdbaaa64bcd2e65345fa8853b117f30ce4a
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-11 10:39:28 +0200

    Add `procreate` method.

--- src/island.rs
+++ src/island.rs
@@ -1,11 +1,12 @@
 use rand::prelude::ThreadRng;
-use crate::animals;
+use rand::Rng;
+use super::animals::*;
 
 pub struct Island<'a> {
     pub year: u16,
     pub geography: Vec<Vec<char>>,
 
-    pub cells: Vec<Vec<(Vec<animals::Herbivore>, Vec<animals::Carnivore>)>>,
+    pub cells: Vec<Vec<(Vec<Herbivore>, Vec<Carnivore>)>>,
     pub inhabited: Vec<(usize, usize)>,
 
     // 'a is a lifetime, meaning that rng lives as long as Island.
@@ -39,8 +40,8 @@ impl Island<'_> {
         let mut cells = geography.iter()
             .map(|row| {
                 row.iter()
-                    .map(|_| (Vec::<animals::Herbivore>::new(),
-                              Vec::<animals::Carnivore>::new())).collect::<Vec<_>>()
+                    .map(|_| (Vec::<Herbivore>::new(),
+                              Vec::<Carnivore>::new())).collect::<Vec<_>>()
             }).collect::<Vec<_>>();
         let mut inhabited = Vec::<(usize, usize)>::new();
 
@@ -54,7 +55,7 @@ impl Island<'_> {
     // Input: vec![((x, y, 'X', Y)]
     // Where x, y is the coordinate
     // and 'X', Y the species and amount; 'h': Herbivore, 'c': Carnivore
-    pub fn add_population(&mut self, rng: &mut ThreadRng, population: Vec<(usize, usize, char, u16)>) {
+    pub fn add_population(&mut self, population: Vec<(usize, usize, char, u16)>) {
         for item in population.iter() {
 
             // Update inhabited vector with current index-pair.
@@ -67,16 +68,16 @@ impl Island<'_> {
             for _ in 0..item.3 {
                 match item.2 {
                     'h' => {
-                        let herbivore = animals::Herbivore {
-                            weight: animals::Herbivore::birthweight(rng),
+                        let herbivore = Herbivore {
+                            weight: Herbivore::birthweight(self.rng),
                             age: 0,
                             fitness: None
                         };
                         cell.0.push(herbivore);
                     },
                     'c' => {
-                        let carnivore = animals::Carnivore {
-                            weight: animals::Carnivore::birthweight(rng),
+                        let carnivore = Carnivore {
+                            weight: Carnivore::birthweight(self.rng),
                             age: 0,
                             fitness: None
                         };
@@ -91,12 +92,52 @@ impl Island<'_> {
         }
     }
 
-    // geography : Contains the terrain types at each index-pair.
-    // cells     : Contains the actual animals of each cell.
-    // inhabited : Contains indices of inhabited cells.
     pub fn procreate(&mut self) {
         for (x, y) in self.inhabited.iter() {
 
+            // Loop through the Herbivoress of the cell:
+            let p_herb = Herbivore::GAMMA *  self.cells[x][y][0].len();
+            for herbivore in self.cells[x][y][0].iter() {
+                if herbivore.weight < Herbivore::PROCREATE {
+                    continue
+                }
+                if self.rng.gen::<f32>() >= herbivore.fitness * p_herb {
+                    continue
+                }
+                let babyweight = Herbivore::birthweight(self.rng);
+                if !herbivore.lose_weight_birth(babyweight) {
+                    continue
+                }
+                self.cells[x][y][0].push(
+                    Herbivore {
+                        weight: babyweight,
+                        age: 0,
+                        fitness: None
+                    }
+                )
+            }
+
+            // Loop through the Carnivores of the cell:
+            let p_carn = Carnivore::GAMMA *  self.cells[x][y][1].len();
+            for carnivore in self.cells[x][y][1].iter() {
+                if carnivore.weight < Carnivore::PROCREATE {
+                    continue
+                }
+                if self.rng.gen::<f32>() >= carnivore.fitness * p_carn {
+                    continue
+                }
+                let babyweight = Carnivore::birthweight(self.rng);
+                if !carnivore.lose_weight_birth(babyweight) {
+                    continue
+                }
+                self.cells[x][y][0].push(
+                    Carnivore {
+                        weight: babyweight,
+                        age: 0,
+                        fitness: None
+                    }
+                )
+            }
         }
     }
 }
