dc849b600336cef457fc2874145d7eb93be6323c
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-15 19:48:27 +0200

    Updated `main.rs` to correspond with changes.

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,37 @@
-use crate::animals::Species::{Carnivore, Herbivore};
-
 pub mod animals;
 pub mod island;
+mod graphics;
+mod benchmark;
+mod simulation;
 
 fn main() {
     let mut rng = rand::thread_rng();
 
+    // let geography: Vec<&str> = vec![
+    //     "WWWWW",
+    //     "WLLLW",
+    //     "WLLLW",
+    //     "WLLLW",
+    //     "WWWWW"
+    // ];
     let geography: Vec<&str> = vec![
-        "WWWWW",
-        "WLLLW",
-        "WLLLW",
-        "WLLLW",
-        "WWWWW"
+             "WWWWWWWWWWWWW",
+             "WWWLHHWWWHHWW",
+             "WWLLLHWLWHLLW",
+             "WWLLLLLLLMLMW",
+             "WWHHLLLHLHMMW",
+             "WHHLLLHWHHLMW",
+             "WWWHHWWWWWMWW",
+             "WWWWWWWWWWWWW",
     ];
 
-    let mut isl = island::Island::new(geography, &mut rng);
-
-    isl.add_population(vec![
-        ((2, 2), Herbivore, 10),
-        ((2, 2), Carnivore, 2)
+    let mut sim = simulation::Simulation::new(
+        geography, &mut rng, "graph.png"
+    );
+    sim.add_population(vec![
+        ((2, 2), animals::Species::Herbivore, 100),
+        ((2, 2), animals::Species::Carnivore, 10)
     ]);
 
-    println!("Metrics: {:#?}", isl.animals());
-
-    isl.yearly_cycle();
-    isl.yearly_cycle();
-    isl.yearly_cycle();
-    isl.yearly_cycle();
-    isl.yearly_cycle();
-
-    let metrics = isl.animals();
-    println!("Metrics: {:#?}", metrics);
-
-
-    let mut keys: Vec<_> = metrics.2.keys().collect();
-    keys.sort();
-    let mut inhabit: Vec<_> = isl.inhabited.iter().collect();
-    inhabit.sort();
-    assert_eq!(keys, inhabit);
+    sim.simulate(1000, true);
 }
