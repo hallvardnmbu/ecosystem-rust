f4163e2d4a0dff20a8aa9f62cc6d692a684fef42
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-05-15 22:13:37 +0200

    Updated benchmarking code. Marginally better.

--- src/benchmark.rs
+++ src/benchmark.rs
@@ -2,39 +2,48 @@
 #[path = "../src/island.rs"] mod island;
 
 fn main() {
-    let mut rng = rand::thread_rng();
+    let mut times = Vec::new();
+    for _ in 0..15 {
+        let mut rng = rand::thread_rng();
+        let geography: Vec<&str> = vec![
+            "WWWWWWWWWWWWWWWWWWWWW",
+            "WHHHHHLLLLWWLLLLLLLWW",
+            "WHHHHHLLLLWWLLLLLLLWW",
+            "WHHHHHLLLLWWLLLLLLLWW",
+            "WWHHLLLLLLLWWLLLLLLLW",
+            "WWHHLLLLLLLWWLLLLLLLW",
+            "WWWWWWWWHWWWWLLLLLLLW",
+            "WHHHHHLLLLWWLLLLLLLWW",
+            "WHHHHHHHHHWWLLLLLLWWW",
+            "WHHHHHMMMMMLLLLLLLWWW",
+            "WHHHHHMMMMMLLLLLLLWWW",
+            "WHHHHHMMMMMLLLLLLLWWW",
+            "WHHHHHMMMMMWWLLLLLWWW",
+            "WHHHHMMMMMMLLLLWWWWWW",
+            "WWHHHHMMMMMMLWWWWWWWW",
+            "WWHHHHMMMMMLLLWWWWWWW",
+            "WHHHHHMMMMMLLLLLLLWWW",
+            "WHHHHMMMMMMLLLLWWWWWW",
+            "WWHHHHMMMMMLLLWWWWWWW",
+            "WWWHHHHLLLLLLLWWWWWWW",
+            "WWWHHHHHHWWWWWWWWWWWW",
+            "WWWWWWWWWWWWWWWWWWWWW",
+        ];
 
-    let geography: Vec<&str> = vec![
-             "WWWWWWWWWWWWW",
-             "WWWLHHWWWHHWW",
-             "WWLLLHWLWHLLW",
-             "WWLLLLLLLMLMW",
-             "WWHHLLLHLHMMW",
-             "WHHLLLHWHHLMW",
-             "WWWHHWWWWWMWW",
-             "WWWWWWWWWWWWW",
-    ];
+        let mut isl = island::Island::new(geography, &mut rng);
 
-    let mut isl = island::Island::new(geography, &mut rng);
+        isl.add_population(vec![
+            ((4, 4), animals::Species::Herbivore, 100),
+            ((4, 4), animals::Species::Carnivore, 10)
+        ]);
 
-    isl.add_population(vec![
-        ((4, 4), animals::Species::Herbivore, 100),
-        ((4, 4), animals::Species::Carnivore, 10)
-    ]);
-
-    // println!("Metrics: {:?}", isl.animals());
-
-    let start = std::time::Instant::now();
-    for i in 0..1000 {
-        isl.yearly_cycle();
-        // if i % 100 == 0 {
-        //     println!("{} iteration, n_animals: {:?} {:?}", i, isl.animals().0, isl.animals().1);
-        // }
+        let start = std::time::Instant::now();
+        for _ in 0..5000 {
+            isl.yearly_cycle();
+        }
+        let duration = start.elapsed();
+        times.push(duration);
     }
-    let duration = start.elapsed();
-    println!("Time elapsed is: {:?}", duration);
-    println!("n_animals: {:?} {:?}", isl.animals().0, isl.animals().1)
-
-    // let metrics = isl.animals();
-    // println!("Metrics: {:#?}", metrics);
+    let mean = times.iter().sum::<std::time::Duration>() / times.len() as u32;
+    println!("Original mean time {:?}", mean);
 }
